bd0f8ca1a9c1a06234d2868c14540330cec602b6
voleter <sayyegh@gmail.com>
2024-02-19 00:15:03 +0300

    Adding Keyboard

---
+++ Assets/Mohammed/MScripts/KeyboardCamera.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using NumericsVector3 = System.Numerics.Vector3;
+
+public class KeyboardFollower : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public Vector3 offsetFromCamera; 
+
+    void Update()
+    {
+        if (cameraTransform != null)
+        {
+            
+            transform.position = cameraTransform.position + cameraTransform.rotation * offsetFromCamera;
+            
+            transform.LookAt(transform.position + cameraTransform.rotation * Vector3.forward, cameraTransform.rotation * Vector3.up);
+        }
+    }
+}
